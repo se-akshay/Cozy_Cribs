cc8855adb0e012327196ebcb2e4f717cddc77242
se-akshay <akshaysingh6225808@gmail.com>
2025-07-01 02:37:41 +0530

    .

--- app.js
+++ app.js
@@ -63,10 +63,6 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.redirect("/listings");
-});
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -85,11 +81,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/listings/category", categoryRouter);
-
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", usersRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 // 404 page not found route
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found..."));

a2828869e07766aa9d7bbb21a44c0cbe31192844
se-akshay <akshaysingh6225808@gmail.com>
2025-07-01 02:30:36 +0530

    ..

--- app.js
+++ app.js
@@ -63,9 +63,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.redirect("/listings");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
